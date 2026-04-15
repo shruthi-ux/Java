6b4fd15f58569dcb989cd0f26abf1c1fc56522a5
shruthi-ux <shruthi122002@gmail.com>
2026-04-15 16:27:15 +0530

    Add files via upload

---
+++ ExampleForOperators.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class ExampleForOperators {
+   public static void main(String[] args) {
+	   
+	   System.out.println("-----Arithmetic Operators-----");
+	   
+	   int num1=30;
+	   int num2=40;
+	   System.out.println("Addition"+(num1+num2));
+	   System.out.println("Substraction"+(num1-12));
+	   System.out.println("Multiplication"+(num2*3));
+	   System.out.println("Division"+(num2/10));
+	   System.out.println("Modulus"+(num2 % 3));
+   }
+}
---
+++ SecondProgram.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class SecondProgram {
+	public static void main(String[] args) {
+		//Variable declaration and initialization
+		int age=20;
+		float height=4.5f;
+		//double h=6.1; single line comment
+		System.out.println("The age of the student " +age);
+		System.out.println("The height of the student " +height);
+		/*------------------------------------------------------
+		 * ----------multi line comment--------------------------
+		 */
+	}
+
+}
